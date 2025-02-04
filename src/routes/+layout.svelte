6e1f51cfbd0624f0376beb70690d34fa715ff230
Eriseldi <eribeq21@htl-shkoder.com>
2025-02-04 12:05:18 +0100

    My second commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<header>
+	<h1>Header</h1>
+</header>
+
+<main>
+	{@render children()}
+</main>
+
+<footer>
+	<h1>Footer</h1>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	let message = $state('Hello!');
+</script>
+
+<h1>Write down something please:</h1>
+<h3>{message}</h3>
+<input type="text" bind:value={message} />
+
+<style>
+	:global(body) {
+		font-family: Arial, sans-serif;
+		background-color: #f4f4f4;
+		color: #333;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		height: 100vh;
+	}
+	input {
+		padding: 0.5rem;
+		font-size: 1rem;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+		width: 100%;
+		max-width: 300px;
+		margin-top: 1rem;
+		outline: none;
+	}
+</style>
